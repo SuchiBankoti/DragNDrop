27b1d8697e6aabf5e5df28ce1ca5e815e879c25e
suchi <suchibankoti95@gmail.com>
2023-06-09 15:30:19 +0530

    implement function for adding items in container

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-function allow_drop(event) {
+document.addEventListener("DOMContentLoaded", addImagesToContainer);
+
+function allow_drop(e) {
   //use prevent default to allow drop
-  event.preventDefault();
+  e.preventDefault();
 }
-function on_dragStart(event) {
-  event.dataTransfer.setData("text", event.target.id);
+function on_dragStart(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
-function on_drop(event) {
+function on_drop(e) {
   //this function takes the id of the target and append it to the dropzone
-  event.preventDefault();
-  let data = event.dataTransfer.getData("text");
+  e.preventDefault();
+  let data = e.dataTransfer.getData("text");
   const draggedTag = document.getElementById(data);
-  event.target.appendChild(draggedTag);
+  e.target.appendChild(draggedTag);
+}
+
+// creates  new image tags and append them to container
+function addImagesToContainer() {
+  const container = document.getElementById("container");
+  for (let i = 1; i <= 8; i++) {
+    const newimg = document.createElement("img");
+    newimg.className = "item";
+    newimg.id = `img${i}`;
+    newimg.alt = "logo";
+    newimg.src = `./items/i${i}.png`;
+    newimg.draggable = true;
+    newimg.addEventListener("dragstart", on_dragStart);
+    container.appendChild(newimg);
+  }
 }
