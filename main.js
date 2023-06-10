c553b3bb35dbb85f3ececa1cf0a75382ac3ebf0b
suchi <suchibankoti95@gmail.com>
2023-06-10 14:47:17 +0530

    change appearnce of the item being dragged

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded", addImagesToContainer);
+document.addEventListener("DOMContentLoaded", addItemsToContainer);
+const main = document.getElementById("main");
+const container = document.getElementById("container");
+const dropzone = document.getElementById("dropzone");
+const body = document.querySelector("body");
 
 // create a message box to give msg after drop
-const body = document.querySelector("body");
 const div = document.createElement("div");
 div.id = "msgbox";
 const p = document.createElement("p");
@@ -15,52 +18,59 @@ function allow_drop(e) {
 }
 function on_dragStart(e) {
   e.dataTransfer.setData("text", e.target.id);
-  e.target.style.opacity = "0.5";
+  e.target.style.background = "white";
 }
 function on_drag(e) {
   e.preventDefault();
-  e.target.style.opacity = "0.5";
+  if (e.dataTransfer.types.includes("text")) {
+    e.target.style.background = "white";
+  } else {
+    e.target.style.background = "grey";
+  }
 }
 function on_dragEnd(e) {
-  e.target.style.opacity = "1";
+  e.target.style.background = "transparent";
 }
 
 function on_drop(e) {
   //this function takes the id of the target and append it to the dropzone
   e.preventDefault();
+  removeDropIcon();
+
   let data = e.dataTransfer.getData("text");
   const draggedTag = document.getElementById(data);
-  e.target.style.opacity = "1";
   e.target.appendChild(draggedTag);
   addMsg();
   setTimeout(() => removeMsg(), 1000);
 }
 
 // creates  new image tags and append them to container
-function addImagesToContainer() {
-  const container = document.getElementById("container");
+function addItemsToContainer() {
   for (let i = 1; i <= 8; i++) {
-    const newimg = document.createElement("img");
-    newimg.className = "item";
-    newimg.id = `img${i}`;
-    newimg.alt = "logo";
-    newimg.src = `./items/i${i}.png`;
-    newimg.draggable = true;
-    newimg.addEventListener("dragend", on_dragEnd);
-    newimg.addEventListener("drag", on_drag);
-    newimg.addEventListener("dragstart", on_dragStart);
-    container.appendChild(newimg);
+    const div = document.createElement("div");
+    div.className = "item";
+    div.draggable = true;
+    div.id = `item${i}`;
+    div.addEventListener("dragstart", on_dragStart);
+    div.addEventListener("drag", on_drag);
+    div.addEventListener("dragend", on_dragEnd);
+    container.appendChild(div);
   }
 }
 
 function addMsg() {
-  const main = document.getElementById("main");
   div.style.display = "block";
   main.style.filter = "blur(5px)";
 }
 function removeMsg() {
   const msgbox = document.getElementById("msgbox");
   msgbox.style.display = "none";
-  const main = document.getElementById("main");
   main.style.filter = "blur(0)";
+}
+
+function removeDropIcon() {
+  const icon = document.getElementById("dropIcon");
+  if (icon) {
+    dropzone.removeChild(icon);
+  }
 }
---
+++ new.js
@@ -0,0 +1,15 @@
+{
+  /* <i data-feather="heart" class="icon"></i>
+          <i data-feather="alert-triangle" class="icon"></i>
+          <i data-feather="bell-off" class="icon"></i>
+          <i data-feather="battery-charging" class="icon"></i>
+          const newimg = document.createElement("img");
+          newimg.className = "item";
+          newimg.id = `img${i}`;
+          newimg.alt = "logo";
+          newimg.src = `./items/i${i}.png`;
+          newimg.draggable = true;
+          newimg.addEventListener("dragend", on_dragEnd);
+          newimg.addEventListener("drag", on_drag);
+          newimg.addEventListener("dragstart", on_dragStart); */
+}
